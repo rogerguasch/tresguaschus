f1b9e616b6b6c25b97d6330a76c121864b209464
Roger Guasch <roger.guasch.rion@gmail.com>
2025-06-11 18:22:21 +0200

    pint and rector

---
+++ config/essentials.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use NunoMaduro\Essentials\Configurables\ForceScheme;
+use NunoMaduro\Essentials\Configurables\Unguard;
+
+return [
+    Unguard::class => false,
+    ForceScheme::class => false,
+];
